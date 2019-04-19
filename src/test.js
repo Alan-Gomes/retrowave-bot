68406f6a961290497b8332eedcde8728c33c0054
Alan Gomes <alangomes11@gmail.com>
2019-04-19 18:08:32 -0300

    Remove arquivo de teste

--- src/test.js
+++
@@ -1,8 +0,0 @@
-const gerador = require('./gerador');
-const fs = require('fs');
-
-gerador.criaImagem('eu vo ve e te aviso').then(buf => {
-  const stream = fs.createWriteStream('text.png');
-  stream.write(buf);
-  stream.end();
-});
